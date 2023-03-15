4e6aaf5106bb35333b6f2e13dc8265e60806a104
Romain Laurent <romain.laurent@cdiscount.com>
2023-03-15 16:30:10 +0100

    feat: 🎸 add HomePage through MUI

---
+++ src/HomePage/index.js
@@ -0,0 +1,42 @@
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Mail } from "@mui/icons-material";
+import Image from "mui-image";
+
+import logo from "../images/1.jpg";
+
+export default function HomePage() {
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="h1">
+            Peak Shirt
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { md: "flex" } }}>
+            <IconButton
+              size="large"
+              aria-label="show 4 new mails"
+              color="inherit"
+            >
+              <Badge badgeContent={4} color="error">
+                <Mail />
+              </Badge>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Image src={logo} duration={0} alt="Peak Shirt Logo" />
+
+      <Typography>Construction en cours…</Typography>
+    </>
+  );
+}
