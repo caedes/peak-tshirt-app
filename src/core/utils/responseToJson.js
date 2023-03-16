c9260a231db22c201619545b1d46eb926e341899
Romain Laurent <romain.laurent@cdiscount.com>
2023-03-16 16:10:49 +0100

    fix: 🐛 udpate responseToJson to handle missing config.json

--- src/core/utils/responseToJson.js
+++ src/core/utils/responseToJson.js
@@ -1,3 +1,11 @@
-export default function responseToJson(response) {
-  return response.json();
+export default async function responseToJson(response) {
+  let data;
+
+  try {
+    data = await response.json();
+  } catch (error) {
+    data = {};
+  }
+
+  return data;
 }
