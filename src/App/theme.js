7438028d06689fdc664e7e0df3cecfaf97a38d83
Romain Laurent <romain.laurent@cdiscount.com>
2023-03-16 10:07:29 +0100

    feat: 🎸 update theme to handle AppBar

--- src/App/theme.js
+++ src/App/theme.js
@@ -1,3 +1,26 @@
 import { createTheme } from "@mui/material/styles";
 
-export default createTheme();
+import { grey, common } from "@mui/material/colors";
+
+/**
+ * Peak Shirt Theme
+ *
+ * Features:
+ * - AppBar is stiky by default
+ */
+export default createTheme({
+  components: {
+    MuiAppBar: {
+      defaultProps: {
+        position: "sticky",
+      },
+      styleOverrides: {
+        root: {
+          backgroundColor: common.white,
+          color: grey[900],
+        },
+      },
+    },
+  },
+});
+// TODO À tester
